95376ebc2569194d05cc7e093ddbb9459a282443
unknown <matheusdasilvagastaldi@gmail.com>
2025-02-27 17:45:16 -0300

    Testando WHEN

---
+++ src/BasicEX/CalculadoraComWhen.kt
@@ -0,0 +1,56 @@
+package BasicEX
+
+private var num1: Int = 0
+private var num2: Int = 0
+
+private var menu = 0
+
+
+fun main() {
+    while (true) {
+        if (menu == 0) {
+            println("\nDigite um número: ")
+            num1 = readln().toInt()
+            println("\nDigite mais um número: ")
+            num2 = readln().toInt()
+            menu = 1
+        } else if (menu == 1) {
+            println(
+                "\nEscolha qual operação matematica gostaria de fazer: " +
+                        "\n1 = Somar" +
+                        "\n2 = Multiplicação" +
+                        "\n3 = Divisão" +
+                        "\n4 = Subtração" +
+                        "\n5 = Trocar número" +
+                        "\n6 = Sair"
+            )
+            val operacao = readLine()?.toIntOrNull()
+
+            when (operacao) {
+                1 -> print("O resultado da soma: $num1 + $num2 é igual a " + soma(num1, num2))
+                2 -> print("O resultado da multiplicação: $num1 * $num2 é igual a " + multi(num1, num2))
+                3 -> print("O resultado da divisão: $num1 / $num2 é igual a " + divisao(num1, num2))
+                4 -> print("O resultado da subtração: $num1 - $num2 é igual a " + subtra(num1, num2))
+                5 -> menu = 0
+                6 -> break
+
+            }
+        }
+    }
+
+    fun soma(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun multi(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun divisao(a: Int, b: Int): Int {
+        return a / b
+    }
+
+    fun subtra(a: Int, b: Int): Int {
+        return a - b
+    }
+}
